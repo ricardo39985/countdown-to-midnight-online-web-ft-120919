50e1586f6e1e7330de3268674123450dd5750930
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-11 00:10:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,5 +9,10 @@ def countdown(starting_value)
 end
 
 def countdown_with_sleep
-
+  while starting_value != 0
+    puts "#{starting_value} SECOND(S)!"
+    sleep(1)
+    starting_value -=1
+  end
+  return "HAPPY NEW YEAR!"
 end
